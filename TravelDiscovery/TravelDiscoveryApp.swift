c710c8e6a15c625d07deca134e126affb6e2be85
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-02 14:43:01 -0600

    Project and gitignore created

---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Alejandro Robles on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
