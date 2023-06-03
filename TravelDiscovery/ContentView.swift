bce13e73a0f05edf5a016c05c4248666d0047d42
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-02 21:36:41 -0600

    Discover categories scrollview

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -10,7 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            Text("Hello World!")
+            ScrollView {
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack(spacing: 8) {
+                        ForEach(0..<5, id: \.self) { num in
+                            VStack(spacing: 4) {
+                                Spacer()
+                                    .frame(width: 50, height: 50)
+                                    .background(Color.gray)
+                                    .cornerRadius(.infinity)
+                                    .shadow(color: .gray, radius: 4, x: 0, y: 2)
+                                Text("Art")
+                                    .font(.system(size: 12, weight: .semibold))
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+            }
             .navigationTitle("Discover")
         }
     }
