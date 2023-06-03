567905a45d0758941f53ad97a99f77e76297f32a
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-03 11:55:59 -0600

    Discover categories refactored

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -117,20 +117,35 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+struct Category: Hashable {
+    let name, imageName: String
+}
+
 struct DiscoverCategoriesView: View {
+    let categories: [Category] = [
+        .init(name: "Art", imageName: "paintpalette.fill"),
+        .init(name: "Sport", imageName: "sportscourt.fill"),
+        .init(name: "Live events", imageName: "music.mic"),
+        .init(name: "Food", imageName: "tray.fill"),
+        .init(name: "History", imageName: "books.vertical.fill"),
+    ]
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 8) {
-                ForEach(0..<5, id: \.self) { num in
-                    VStack(spacing: 4) {
-                        Spacer()
-                            .frame(width: 50, height: 50)
+            HStack(alignment: .top, spacing: 14) {
+                ForEach(categories, id: \.self) { category in
+                    VStack(spacing: 8) {
+                        Image(systemName: category.imageName)
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(width: 64, height: 64)
                             .background(Color.gray)
                             .cornerRadius(.infinity)
                             .shadow(color: .gray, radius: 4, x: 0, y: 2)
-                        Text("Art")
+                        Text(category.name)
                             .font(.system(size: 12, weight: .semibold))
+                            .multilineTextAlignment(.center)
                     }
+                    .frame(width: 68)
                 }
             }
             .padding(.horizontal)
