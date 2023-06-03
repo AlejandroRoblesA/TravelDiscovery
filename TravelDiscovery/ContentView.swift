52f1b601daef01bd6fc04616c44f3ed2619bda91
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-03 11:05:35 -0600

    Treanding creators View Module

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 DiscoverCategoriesView()
                 PopularDestinationsView()
                 PopularRestaurantsView()
+                TrendingCreatorsView()
             }
             .navigationTitle("Discover")
         }
@@ -71,6 +72,36 @@ struct PopularRestaurantsView: View {
                         .background(Color.gray)
                         .cornerRadius(5)
                         .shadow(color: .gray,radius: 4, x: 0, y: 2)
+                        .padding(.bottom)
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct TrendingCreatorsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular Destinations")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }
+            .padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(0..<15, id: \.self) { num in
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: 0, y: 2)
                         .padding(.bottom)
                     }
                 }
