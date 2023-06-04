35eaf6223fa0d065fcfee4db6ed2da54797ddd4c
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-04 13:16:03 -0600

    Trending Creators Module Refactored

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -139,11 +139,19 @@ struct PopularRestaurantsView: View {
     }
 }
 
+struct Users: Hashable {
+    let name, imageName: String
+}
+
 struct TrendingCreatorsView: View {
+    let users: [Users] = [
+        .init(name: "Amy Adams", imageName: "amy"),
+        .init(name: "Billy", imageName: "billy"),
+        .init(name: "Sam Smith", imageName: "sam")]
     var body: some View {
         VStack {
             HStack {
-                Text("Popular Destinations")
+                Text("Trending Creators")
                     .font(.system(size: 14, weight: .semibold))
                 Spacer()
                 Text("See all")
@@ -153,13 +161,20 @@ struct TrendingCreatorsView: View {
             .padding(.top)
             
             ScrollView(.horizontal) {
-                HStack(spacing: 8) {
-                    ForEach(0..<15, id: \.self) { num in
-                        Spacer()
-                            .frame(width: 50, height: 50)
-                            .background(Color.gray)
-                            .cornerRadius(.infinity)
-                            .shadow(color: .gray, radius: 4, x: 0, y: 2)
+                HStack(alignment: .top, spacing: 12) {
+                    ForEach(users, id: \.self) { user in
+                        VStack {
+                            Image(user.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 60, height: 60)
+                                .cornerRadius(.infinity)
+                            Text(user.name)
+                                .font(.system(size: 11, weight: .semibold))
+                                .multilineTextAlignment(.center)
+                        }
+                        .frame(width: 60)
+                        .shadow(color: .gray, radius: 4, x: 0, y: 2)
                         .padding(.bottom)
                     }
                 }
