443fbdb29a6210821c6fee34ba192a13759f89ee
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-05 18:23:50 -0600

    Navigation Link To Category Detail View

--- TravelDiscovery/Discover/DiscoverCategoriesView.swift
+++ TravelDiscovery/Discover/DiscoverCategoriesView.swift
@@ -44,6 +44,25 @@ struct DiscoverCategoriesView: View {
     }
 }
 
+struct CategoryDetailView: View {
+    var body: some View {
+        ScrollView {
+            ForEach(0..<5, id: \.self) { _ in
+                VStack(alignment: .leading, spacing: 0) {
+                    Image("art1")
+                        .resizable()
+                        .scaledToFill()
+                    Text("Demo")
+                        .font(.system(size: 12, weight: .semibold))
+                        .padding()
+                }
+                .asTile()
+                .padding()
+            }
+        }
+    }
+}
+
 struct DiscoverCategoriesView_Previews: PreviewProvider {
     static var previews: some View {
         ZStack {
