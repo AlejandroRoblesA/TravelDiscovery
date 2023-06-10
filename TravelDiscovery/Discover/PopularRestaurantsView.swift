57f4e25a51fd9728092bfff73be0e548b4d7546b
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-09 20:40:44 -0600

    RestaurantTile View

--- TravelDiscovery/Discover/PopularRestaurantsView.swift
+++ TravelDiscovery/Discover/PopularRestaurantsView.swift
@@ -25,33 +25,7 @@ struct PopularRestaurantsView: View {
             ScrollView(.horizontal) {
                 HStack(spacing: 8) {
                     ForEach(restaurants, id: \.self) { restaurant in
-                        HStack(spacing: 8) {
-                            Image(restaurant.image)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 60, height: 60)
-                                .clipped()
-                                .cornerRadius(5)
-                                .padding(.leading, 6)
-                                .padding(.vertical, 6)
-                            VStack(alignment: .leading, spacing: 6) {
-                                HStack {
-                                    Text(restaurant.name)
-                                    Spacer()
-                                    Button(action: {}) {
-                                        Image(systemName: "ellipsis")
-                                            .foregroundColor(.gray)
-                                    }
-                                }
-                                HStack {
-                                    Image(systemName: "star.fill")
-                                        Text("4.7 • Sushi • $$")
-                                }
-                                Text("Tokyo, Japan")
-                            }
-                            .font(.system(size: 12, weight: .semibold))
-                            Spacer()
-                        }
+                        RestaurantTile(restaurant: restaurant)
                         .frame(width: 240)
                         .modifier(TileModifier())
                         .padding(.bottom)
@@ -59,6 +33,39 @@ struct PopularRestaurantsView: View {
                 }
                 .padding(.horizontal)
             }
+        }
+    }
+}
+
+struct RestaurantTile: View {
+    let restaurant: Restaurant
+    var body: some View {
+        HStack(spacing: 8) {
+            Image(restaurant.image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 60, height: 60)
+                .clipped()
+                .cornerRadius(5)
+                .padding(.leading, 6)
+                .padding(.vertical, 6)
+            VStack(alignment: .leading, spacing: 6) {
+                HStack {
+                    Text(restaurant.name)
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "ellipsis")
+                            .foregroundColor(.gray)
+                    }
+                }
+                HStack {
+                    Image(systemName: "star.fill")
+                        Text("4.7 • Sushi • $$")
+                }
+                Text("Tokyo, Japan")
+            }
+            .font(.system(size: 12, weight: .semibold))
+            Spacer()
         }
     }
 }
