b6ea719aeb2b42578fd1843c010c32dd147aa162
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-10 17:31:41 -0600

    Dishes cell created

--- TravelDiscovery/Discover/Restaurant/RestaurantDetailsView.swift
+++ TravelDiscovery/Discover/Restaurant/RestaurantDetailsView.swift
@@ -93,39 +93,46 @@ struct RestaurantDetailsView: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 16) {
                     ForEach(vm.details?.popularDishes ?? [], id: \.self) { dish in
-                        VStack(alignment: .leading) {
-                            ZStack(alignment: .bottomLeading) {
-                                KFImage(URL(string: dish.photo))
-                                    .resizable()
-                                    .scaledToFill()
-                                    .cornerRadius(5)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 5)
-                                            .stroke(Color.gray)
-                                    )
-                                    .shadow(radius: 2)
-                                    .padding(.vertical, 4)
-                                LinearGradient(colors: [Color.clear, Color.black], startPoint: .center, endPoint: .bottom)
-                                Text(dish.price)
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 13, weight: .bold))
-                                    .padding(.horizontal, 6)
-                                    .padding(.bottom, 4)
-                            }
-                            .frame(height: 120)
-                            .cornerRadius(5)
-                            Text(dish.name)
-                                .font(.system(size: 14, weight: .bold))
-                            Text("\(dish.numPhotos) photos")
-                                .foregroundColor(.gray)
-                                .font(.system(size: 12, weight: .regular))
-                        }
+                        DishCell(dish: dish)
                     }
                 }
                 .padding(.horizontal)
             }
         }
         .navigationBarTitle("Restaurant Details", displayMode: .inline)
+    }
+}
+
+struct DishCell: View {
+    let dish: Dish
+    var body: some View {
+        VStack(alignment: .leading) {
+            ZStack(alignment: .bottomLeading) {
+                KFImage(URL(string: dish.photo))
+                    .resizable()
+                    .scaledToFill()
+                    .cornerRadius(5)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.gray)
+                    )
+                    .shadow(radius: 2)
+                    .padding(.vertical, 4)
+                LinearGradient(colors: [Color.clear, Color.black], startPoint: .center, endPoint: .bottom)
+                Text(dish.price)
+                    .foregroundColor(.white)
+                    .font(.system(size: 13, weight: .bold))
+                    .padding(.horizontal, 6)
+                    .padding(.bottom, 4)
+            }
+            .frame(height: 120)
+            .cornerRadius(5)
+            Text(dish.name)
+                .font(.system(size: 14, weight: .bold))
+            Text("\(dish.numPhotos) photos")
+                .foregroundColor(.gray)
+                .font(.system(size: 12, weight: .regular))
+        }
     }
 }
 
