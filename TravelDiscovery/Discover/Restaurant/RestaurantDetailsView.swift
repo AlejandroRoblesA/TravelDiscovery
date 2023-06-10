5cd1c461b900ec8ac9f92fdc2530dcab501fa13a
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-10 17:26:41 -0600

    Popular dishes price showed above the dishes image

--- TravelDiscovery/Discover/Restaurant/RestaurantDetailsView.swift
+++ TravelDiscovery/Discover/Restaurant/RestaurantDetailsView.swift
@@ -94,17 +94,26 @@ struct RestaurantDetailsView: View {
                 HStack(spacing: 16) {
                     ForEach(vm.details?.popularDishes ?? [], id: \.self) { dish in
                         VStack(alignment: .leading) {
-                            KFImage(URL(string: dish.photo))
-                                .resizable()
-                                .scaledToFill()
-                                .frame(height: 80)
-                                .cornerRadius(5)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .stroke(Color.gray)
-                                )
-                                .shadow(radius: 2)
-                                .padding(.vertical, 4)
+                            ZStack(alignment: .bottomLeading) {
+                                KFImage(URL(string: dish.photo))
+                                    .resizable()
+                                    .scaledToFill()
+                                    .cornerRadius(5)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .stroke(Color.gray)
+                                    )
+                                    .shadow(radius: 2)
+                                    .padding(.vertical, 4)
+                                LinearGradient(colors: [Color.clear, Color.black], startPoint: .center, endPoint: .bottom)
+                                Text(dish.price)
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 13, weight: .bold))
+                                    .padding(.horizontal, 6)
+                                    .padding(.bottom, 4)
+                            }
+                            .frame(height: 120)
+                            .cornerRadius(5)
                             Text(dish.name)
                                 .font(.system(size: 14, weight: .bold))
                             Text("\(dish.numPhotos) photos")
