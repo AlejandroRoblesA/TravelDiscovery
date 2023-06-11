1e4490430f7e3ec889223f84b977c443a8e706bc
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-11 12:28:11 -0600

    LazyVGrid filled with a placeholder

--- TravelDiscovery/Discover/Restaurant/RestaurantPhotosView.swift
+++ TravelDiscovery/Discover/Restaurant/RestaurantPhotosView.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 struct RestaurantPhotosView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVGrid(columns: [
+                GridItem(.fixed(125), spacing: 4),
+                GridItem(.fixed(125), spacing: 4),
+                GridItem(.fixed(125))
+            ], spacing: 4) {
+                ForEach(0..<14, id: \.self) { _ in
+                    Text("Placeholder")
+                        .padding()
+                        .background(Color.red)
+                }
+            }
+        }
+        .navigationBarTitle("All photos", displayMode: .inline)
     }
 }
 
 struct RestaurantPhotosView_Previews: PreviewProvider {
     static var previews: some View {
-        RestaurantPhotosView()
+        NavigationView {
+            RestaurantPhotosView()
+        }
     }
 }
